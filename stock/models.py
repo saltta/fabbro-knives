26db2b235402103278adf189748fa7ecc84852f3
saltta <trottsal@gmail.com>
2023-01-09 11:47:12 +0000

    added brand model

--- stock/models.py
+++ stock/models.py
@@ -11,15 +11,25 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    
+    brand = models.ForeignKey('Brand', null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return self.name
