bc4789f42dcb2424a01934237e094676cbb474d3
saltta <trottsal@gmail.com>
2023-01-13 11:25:30 +0000

    added toast templates

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from stock.models import Product
 
 # Create your views here.
 
@@ -12,6 +14,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a specific quantity of an item to the basket """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -20,6 +23,7 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket')
     
     request.session['basket'] = basket
     return redirect(redirect_url)
