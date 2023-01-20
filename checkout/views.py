00aa3313f2b3e3a65220de1abd2f051623b2dd0e
saltta <trottsal@gmail.com>
2023-01-20 13:13:42 +0000

    added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+from django.http import HttpResponse
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
