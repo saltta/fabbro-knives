e282e7624eb23c5ded6c09c2c759df667bb97600
saltta <trottsal@gmail.com>
2023-01-20 13:40:33 +0000

    change

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-from django.http import HttpResponse
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
