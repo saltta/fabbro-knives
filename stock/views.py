4042b80f9e6235ff342c155dbb8aeb40a60c0dbd
saltta <trottsal@gmail.com>
2023-01-24 14:03:24 +0000

    add product functionality

--- stock/views.py
+++ stock/views.py
@@ -73,7 +73,17 @@ def product_info(request, product_id):
 
 def add_product(request):
     """ Add a product to the shop """
-    form = ProductForm
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Sucessfully added product')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
