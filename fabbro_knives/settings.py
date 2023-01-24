b488b5d4402bc3da3722d3359135f284443cbac6
saltta <trottsal@gmail.com>
2023-01-24 16:21:23 +0000

    deployment

--- fabbro_knives/settings.py
+++ fabbro_knives/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bffozt0@g_jg^v2_*p)_tob3cmg&l$j_*#*qocyszxpq2m5#b7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fabbro.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'fabbro_knives.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
