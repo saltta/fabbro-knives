30f193ec1f5c0960d19abeaa4eef8edfcc569b9e
saltta <trottsal@gmail.com>
2023-01-25 14:07:07 +0000

    hidden secret key, set debug

--- fabbro_knives/settings.py
+++ fabbro_knives/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bffozt0@g_jg^v2_*p)_tob3cmg&l$j_*#*qocyszxpq2m5#b7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fabbro.herokuapp.com', 'localhost']
 
