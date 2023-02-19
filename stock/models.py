b221004cadda19e85cdbad10c7bb2e02c68c763d
saltta <trottsal@gmail.com>
2023-02-19 13:37:09 +0000

    updated brand model to add image

--- stock/models.py
+++ stock/models.py
@@ -20,6 +20,7 @@ class Brand(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     location = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
